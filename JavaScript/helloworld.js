8872f6fa7fd10b6347c6d72c7d7346a3fe7a5bfb
Ibrahim6898 <ibrahimyakubuahmad58@gmail.com>
2023-11-06 13:47:46 +0100

    corection

--- JavaScript/helloworld.js
+++ JavaScript/helloworld.js
@@ -2,5 +2,7 @@ let name;
 $name = "Ahmad";
 console.log($name);
 
-let Carname = "Lexus", Cityname = "Maiduguri", Streetname = "Kandahar";
+let Carname = "Lexus"; 
+let Cityname = "Maiduguri"; 
+let Streetname = "Kandahar";
 console.log("I have a ", Carname, "car model.", "I live at ", Streetname, " street of", Cityname)
