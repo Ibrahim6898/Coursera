7148b8daa896359dbb9214fdf292e034b8fdfa4b
Ibrahim6898 <ibrahimyakubuahmad58@gmail.com>
2023-11-06 13:44:17 +0100

    variables

--- JavaScript/helloworld.js
+++ JavaScript/helloworld.js
@@ -2,5 +2,5 @@ let name;
 $name = "Ahmad";
 console.log($name);
 
-let Carname = Lexus, Cityname = Maiduguri, Streetname = Kandahar;
+let Carname = "Lexus", Cityname = "Maiduguri", Streetname = "Kandahar";
 console.log("I have a ", Carname, "car model.", "I live at ", Streetname, " street of", Cityname)
