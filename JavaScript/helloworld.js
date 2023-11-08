f3d1dad9f69c712fa62a9f410024c05e47cff095
Ibrahim6898 <ibrahimyakubuahmad58@gmail.com>
2023-11-08 20:35:21 +0100

    loops

--- JavaScript/helloworld.js
+++ JavaScript/helloworld.js
@@ -40,3 +40,33 @@ longString += " a"
 longString += " time"
 longString += "..."
 console.log(longString)
+
+let num1 = 2
+let num2 = 5
+let test1 = num1 % 2
+let test2 = num2 % 2
+result1 = test1 == 0;
+result2 = test2 == 0;
+console.log("Is", num1, "an even number?", result1)
+console.log("Is", num2, "an even number?", result2)
+
+for (let i = 1; i < 2; i++){
+    // console.log(year)
+    for (let j = 0; j <= 10; j++){
+        console.log(i, "times ", j, "=", i*j)
+    }
+}
+
+
+for (let i = 100; i > 10; i = i - 10){
+    for (let j = 10; j > 4; j = j - 5)
+        console.log(i+ " divided by " +j+ " equals " +i/j)
+}
+
+
+var cubes = 'ABCDEFG';
+//styling console output using CSS with a %c format specifier
+for (var i = 0; i < cubes.length; i++) {
+    var styles = "font-size: 40px; border-radius: 10px; border: 1px solid blue; background: pink; color: purple";
+    console.log("%c" + cubes[i], styles)
+}
